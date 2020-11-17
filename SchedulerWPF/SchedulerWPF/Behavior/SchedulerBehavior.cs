5eb7cf56e863c9fbf02851c806c8a8ae5f575286
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-17 10:06:14 +0530

    WPF-55615 - Sample and screenshot added.

---
+++ SchedulerWPF/SchedulerWPF/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Scheduler;
+
+namespace WpfScheduler
+{
+    public class SchedulerBehavior : Behavior<SfScheduler>
+    {
+        SfScheduler scheduler;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            scheduler = this.AssociatedObject;
+            this.AssociatedObject.TimelineViewSettings.TimeInterval = new System.TimeSpan(1, 0, 0);
+
+            TimelineViewSettings timelineViewSettings = new TimelineViewSettings();
+            timelineViewSettings.StartHour = 0;
+            timelineViewSettings.EndHour = 23;
+            timelineViewSettings.TimeRulerFormat = string.Format("'Room'") + " " + "HH";
+            this.AssociatedObject.TimelineViewSettings = timelineViewSettings;
+
+            DaysViewSettings daysViewSettings = new DaysViewSettings();
+            daysViewSettings.StartHour = 0;
+            daysViewSettings.EndHour = 23;
+            daysViewSettings.TimeRulerFormat = string.Format("'Room'") + " " + "HH";
+            this.AssociatedObject.DaysViewSettings = daysViewSettings;
+        }
+    }
+}
+
+
+
---
+++ SchedulerWPF/SchedulerWPF/ViewModel/SchedulerViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.UI.Xaml.Scheduler;
+using System;
+using System.Windows.Media;
+
+namespace WpfScheduler
+{
+    public class SchedulerViewModel
+    {
+        public ScheduleAppointmentCollection AppointmentCollection { get; set; } = new ScheduleAppointmentCollection();
+        public SchedulerViewModel()
+        {
+            //Adding schedule appointment in schedule appointment collection 
+            ScheduleAppointment appointment1 = new ScheduleAppointment()
+            {
+                StartTime = new DateTime(2020, 11, 10, 10, 0, 0),
+                EndTime = new DateTime(2020, 11, 10, 11, 0, 0),
+                Subject = "Booked",
+                AppointmentBackground = new SolidColorBrush(Colors.Red),
+            };
+            //Creating recurrence rule
+            appointment1.RecurrenceRule = "FREQ=DAILY;INTERVAL=2;COUNT=10";
+            //Adding schedule appointment in the schedule appointment collection
+            AppointmentCollection.Add(appointment1);
+        }
+    }
+}
+
+    
+
+
